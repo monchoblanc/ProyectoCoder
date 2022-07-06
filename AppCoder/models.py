d0956113ed1f893d4b2cb49e4ec3c683a8bc6e53
monchoblanc <monchoblanc@gmail.com>
2022-07-06 12:58:48 -0300

    hasta clase20

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -4,19 +4,27 @@ from django.db import models
 class Curso(models.Model):
     nombre= models.CharField(max_length=50)
     comision=models.IntegerField()
+    def __str__(self):
+        return self.nombre+'  '+str(self.comision)
 
 class Estudiante(models.Model):
     nombre= models.CharField(max_length=50)
     apellido= models.CharField(max_length=50)
     email= models.EmailField()
+    def __str__(self):
+        return self.nombre+' '+self.apellido+' '+str(self.email)
 
 class Profesor(models.Model):
     nombre= models.CharField(max_length=50)
     apellido= models.CharField(max_length=50)
     email= models.EmailField()
     profesion= models.CharField(max_length=50)
+    def __str__(self):
+         return self.nombre+' '+self.apellido+' '+str(self.email)+' '+ self.profesion
 
 class Entregable(models.Model):
     nombre= models.CharField(max_length=50)
     fecha_entrega= models.DateField()
     entregado=models.BooleanField()
+    def __str__(self):
+        return self.nombre+' '+str(self.fecha_entrega)+' '+'entregado:'+str(self.entregado)
--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from AppCoder.views import * #en clase estaba como: from .views import*
 
 urlpatterns = [
-    path('', inicio),         #en filminas era: path('cursos/', views.cursos), e idem en todas....
-    path('cursos/', cursos),
-    path('profesores/', profesores),
-    path('estudiantes/', estudiantes),
-    path('entregables/', entregables),
-    
+    path('', inicio, name='inicio'),         #en filminas era: path('cursos/', views.cursos), e idem en todas....
+    path('cursos/', cursos, name='cursos'), #este name es p q lo tome en el padre.html como referencia al url en el boton
+    path('profesores/', profesores, name='profesores'),
+    path('estudiantes/', estudiantes, name='estudiantes'),
+    path('entregables/', entregables, name='entregables'),
+    #me falta poner el padre aca? NO! no hace falta
 ]
